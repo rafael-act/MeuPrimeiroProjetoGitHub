09b352a496e890eb951eacd008c27e4bd6a45acf
Rafael Castro <peo_rcastro@uolinc.com>
2021-06-23 09:46:30 -0300

    Primeira Atualização da minha solução

---
+++ AloGitHub/AloGitHub/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace AloGitHub
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Primeiro Teste com o GitHub");
+            Console.ReadKey();
+        }
+    }
+}
